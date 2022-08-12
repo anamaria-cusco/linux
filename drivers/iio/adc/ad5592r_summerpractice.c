aab3474d70b4c1e4123c2641c6f302a4043dd23d
Ana-Maria Cusco <anamaria_cusco@yahoo.com>
2022-08-12 09:28:40 +0300

    drivers:adc:ad5592r Add regmap support

--- drivers/iio/adc/ad5592r_summerpractice.c
+++ drivers/iio/adc/ad5592r_summerpractice.c
@@ -8,13 +8,41 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/regmap.h>
+
+struct ad5592r_state{
+	struct regmap *regmap
+};
+
+static int ad5592r_reg_access(struct iio_dev *indio_dev,
+				  unsigned reg, unsigned writeval,
+				  unsigned *readval)
+{
+    struct ad5592r_state *st = iio_priv(indio_dev);
+    if(readval)
+        return regmap_read(st->regmap, reg, readval);
+    return regmap_write(st->regmap, reg, writeval);
+}
 static const struct iio_info ad5592r_info = {
+	.debugfs_reg_access=&ad5592r_reg_access,
+
+};
+
+static const struct regmap_config ad5592r_regmap_cfg = {
+	.reg_bits=5,
+	.val_bits=11,
 };
 
 static int ad5592r_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
-	indio_dev=devm_iio_device_alloc(&spi->dev,0);
+	struct ad5592r_state *st;
+	indio_dev=devm_iio_device_alloc(&spi->dev,sizeof(*st));
+	st=iio_priv(indio_dev);
+	st->regmap=devm_regmap_init_spi(spi, &ad5592r_regmap_cfg);
+	if(IS_ERR(st->regmap)){
+		return PTR_ERR(st->regmap);
+	}
 	if (!indio_dev)
 		return -ENOMEM;
 
