c01ab552008052c6fefca0d568fe4960b40b8767
chegbeli <ciprian.hegbeli@analog.com>
2022-08-15 15:24:50 +0300

    iio:adc:ad5592r_s added SPI functions
    Added the first stepts to access the SPI registers of the ADC
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- drivers/iio/adc/ad5592r_summer.c
+++ drivers/iio/adc/ad5592r_summer.c
@@ -1,6 +1,91 @@
+
+#include <linux/bitfield.h>
+#include <linux/delay.h>
 #include <linux/spi/spi.h>
 #include <linux/module.h>
 #include <linux/iio/iio.h>
+#include <asm/unaligned.h>
+
+#define ADI_AD5592R_REG_ADC_SEQ		0x2
+#define ADI_AD5592R_REG_GPIO_CTL	0x3
+#define ADI_AD5592R_REG_ADC_PIN_CFG	0x4
+#define ADI_AD5592R_REG_READBACK	0x7
+#define ADI_AD5592R_REG_PD		0xB
+#define ADI_AD5592R_REG_RST		0xF
+
+#define ADI_AD5592R_PD_EN_REF		BIT(9)
+#define ADI_AD5592R_DAC_BIT		BIT(15)
+#define ADI_AD5592R_READB_EN_BIT	BIT(6)
+#define ADI_AD5592R_ADDR_MASK		GENMASK(14, 11)
+#define ADI_AD5592R_CTL_VAL_MASK	GENMASK(10, 0)
+#define ADI_AD5592R_READB_REG_MASK	GENMASK(5, 2)
+
+#define ADI_AD5592R_RST_VAL		0xDAC
+#define ADI_AD5592R_ADC_PINS		BIT(3) | BIT(1)
+
+struct adi_ad5592r_state {
+	struct spi_device *spi;
+};
+
+static int adi_ad5592r_write_ctr(struct adi_ad5592r_state *st,
+				 u8 reg, 
+				 u16 val)
+{
+	u16 msg = 0;
+	__be16 tx;
+
+	msg |= FIELD_PREP(ADI_AD5592R_ADDR_MASK, reg);
+	msg |= FIELD_PREP(ADI_AD5592R_CTL_VAL_MASK, val);
+
+	put_unaligned_be16(msg, &tx);
+
+	return spi_write(st->spi, &tx, sizeof(tx));
+}
+
+static int adi_ad5592r_nop(struct adi_ad5592r_state *st, __be16 *buff)
+{
+	struct spi_transfer xfer = {
+		.tx_buf = 0,
+		.rx_buf = buff,
+		.len = 2
+	};
+
+	return spi_sync_transfer(st->spi, &xfer, 1);
+}
+
+static int adi_ad5592r_read_ctr(struct adi_ad5592r_state *st,
+				u8 reg,
+				u16 *val)
+{
+	u16 msg = 0;
+	__be16 tx;
+	__be16 rx;
+	int ret;
+
+	msg |= FIELD_PREP(ADI_AD5592R_ADDR_MASK, ADI_AD5592R_REG_READBACK);
+	msg |= ADI_AD5592R_READB_EN_BIT;
+	msg |= FIELD_PREP(ADI_AD5592R_READB_REG_MASK, reg);
+
+	put_unaligned_be16(msg, &tx);
+
+	ret = spi_write(st->spi, &tx, sizeof(tx));
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Fail spi write at read ctl reg");
+		return ret;
+	}
+
+	ret = adi_ad5592r_nop(st, &rx);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Fail nop at read ctl reg");
+		return ret;
+	}
+
+	*val = get_unaligned_be16(&rx);
+
+	return 0;
+}
 
 static int adi_ad5592r_read_raw(struct iio_dev *indio_dev,
 				struct iio_chan_spec const *chan,
@@ -75,18 +160,21 @@ static const struct iio_chan_spec adi_ad5592r_channels[] = {
 static int adi_ad5592r_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
+	struct adi_ad5592r_state *st;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if(!indio_dev)
 	{
 		return -ENOMEM;
 	}
+	st = iio_priv(indio_dev);
 
 	indio_dev->name = "ad5592r";
 	indio_dev->channels = adi_ad5592r_channels;
 	indio_dev->num_channels = ARRAY_SIZE(adi_ad5592r_channels);
 	indio_dev->info = &adi_ad5592r_info;
 
+	st->spi = spi;
 	dev_info(&spi->dev, "ad5592r Probed");
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
