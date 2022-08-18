e6a72c8c8b471816aa139345f07fd85d0693803d
chegbeli <ciprian.hegbeli@analog.com>
2022-08-18 13:14:38 +0300

    iio:adc:ad5592r_s added debugfs and init function
    Activated the debugfs of the iio for easier read and write to the
    control registers.
    Added a init function with the reset sequence to it.
    
    Activate debugfs in menuconfig:
    make menuconfig
         [*]DEBUG_FS
    
    User-space:
    root@analog:~# echo 0x4 0xA > /sys/kernel/debug/iio/iio\:device0/direct_reg_access
    root@analog:~# cat /sys/kernel/debug/iio/iio\:device0/direct_reg_access
    0xA
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- drivers/iio/adc/ad5592r_summer.c
+++ drivers/iio/adc/ad5592r_summer.c
@@ -8,13 +8,16 @@
 
 #include <asm/unaligned.h>
 #include <linux/bitfield.h>
-#include <linux/spi/spi.h>
-#include <linux/module.h>
+#include <linux/delay.h>
 #include <linux/iio/iio.h>
+#include <linux/module.h>
+#include <linux/spi/spi.h>
 
 #define ADI_AD5592R_REG_READBACK	0x7
 #define	  ADI_AD5592R_MASK_RB_EN	BIT(6)
-#define	  ADI_AD5592R_MASK_REG_RB	GENMASK(5, 2)	
+#define	  ADI_AD5592R_MASK_REG_RB	GENMASK(5, 2)
+#define ADI_AD5592R_REG_RESET		0xF
+#define   ADI_AD5592R_VAL_RESET		0x5AC
 
 #define ADI_AD5592R_ADDR_MASK		GENMASK(14, 11)
 #define ADI_AD5592R_VAL_MASK		GENMASK(10, 0)
@@ -117,9 +120,33 @@ static int adi_ad5592r_write_raw(struct iio_dev *indio_dev,
 
 	return -EINVAL;
 }
+
+static int adi_ad5592r_reg_access(struct iio_dev *indio_dev,
+				  unsigned reg, unsigned writeval,
+				  unsigned *readval)
+{
+	struct adi_ad5592r_state *st = iio_priv(indio_dev);
+	u16 read;
+	int ret;
+
+	if (readval) {
+		ret = adi_ad5592r_read_ctr(st, reg, &read);
+		if(ret){
+			dev_err(&st->spi->dev, "DBG read failed");
+			return ret;
+		}
+		dev_info(&st->spi->dev, "read_reg=0x%x", read);
+		*readval = read;
+		return ret;
+	}
+
+	return 	adi_ad5592r_write_ctr(st, reg, writeval);
+}
+
 static const struct iio_info adi_ad5592r_info = {
 	.read_raw = &adi_ad5592r_read_raw,
 	.write_raw = &adi_ad5592r_write_raw,
+	.debugfs_reg_access = &adi_ad5592r_reg_access,
 };
 
 static const struct iio_chan_spec adi_ad5592r_channels[] = {
@@ -153,10 +180,29 @@ static const struct iio_chan_spec adi_ad5592r_channels[] = {
 	},
 };
 
+static int adi_ad5592r_init(struct iio_dev *indio_dev)
+{
+	struct adi_ad5592r_state *st = iio_priv(indio_dev);
+	int ret;
+
+	//reset
+	ret = adi_ad5592r_write_ctr(st, ADI_AD5592R_REG_RESET, 
+				    ADI_AD5592R_VAL_RESET);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Reset Failed");
+		return ret;
+	}
+	usleep_range(250, 300);
+
+	return 0;	
+}
+
 static int adi_ad5592r_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct adi_ad5592r_state *st;
+	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if(!indio_dev)
@@ -171,6 +217,13 @@ static int adi_ad5592r_probe(struct spi_device *spi)
 	indio_dev->info = &adi_ad5592r_info;
 
 	st->spi = spi;
+
+	ret = adi_ad5592r_init(indio_dev);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Init Failed");
+		return ret;
+	}
 
 	dev_info(&spi->dev, "ad5592r Probed");
 
