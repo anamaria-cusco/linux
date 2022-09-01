dc114889ce24852e13586b4865feb2cf03fe583f
Ana-Maria Cusco <anamaria_cusco@yahoo.com>
2022-09-01 15:01:17 +0300

    iio:amplifiers:adrf5740 First version of adrf5740 attenuator driver

--- drivers/iio/amplifiers/adrf5740.c
+++ drivers/iio/amplifiers/adrf5740.c
@@ -9,6 +9,133 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 #include <linux/platform_device.h>
+#include <linux/err.h>
+#include <linux/gpio/consumer.h>
+
+struct  adrf5740_chip_info {
+        const char			*name;
+	const struct iio_chan_spec	*channels;
+	unsigned int			num_channels;
+	unsigned int			num_gpios;
+	int				gain_min;
+	int				gain_max;
+	int				default_gain;
+};
+
+struct  adrf5740_state {
+	struct	regulator *reg;
+	// struct mutex lock; /* protect sensor state */
+	struct	adrf5740_chip_info *chip_info;
+	struct	gpio_descs *gpios;
+	u32	gain;
+};
+
+static const struct iio_chan_spec adrf5740_channels[] = {
+	{
+		.type = IIO_VOLTAGE,						\
+	        .output = 1,							\
+	        .indexed = 1,							\
+	        .channel = 0,						        \
+	        .info_mask_separate = BIT(IIO_CHAN_INFO_HARDWAREGAIN),		\
+	}, 
+};
+
+static struct adrf5740_chip_info adrf5740_chip_info = {
+		.name = "adrf5740",
+		.channels = adrf5740_channels,
+		.num_channels = ARRAY_SIZE(adrf5740_channels),
+		.num_gpios = 4,
+		.gain_min = 0,
+		.gain_max = 22000,
+		.default_gain = 0xB, /* set default gain -22dB*/
+};
+
+
+static int adrf5740_read_raw(struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				int *val,
+				int *val2,
+				long mask)
+{
+        struct adrf5740_state *st = iio_priv(indio_dev);
+        int code, gain = 0;
+	
+
+	switch (mask) {
+	case IIO_CHAN_INFO_HARDWAREGAIN:
+                code = st->gain;
+                gain = code * -2000; /* 2dB/LSB */
+                *val = gain / 1000;
+		*val2 = (gain % 1000) * 1000; /* micro part */
+                return IIO_VAL_INT_PLUS_MICRO_DB;
+		
+	}
+
+	return -EINVAL;
+}
+
+static int adrf5740_write(struct iio_dev *indio_dev, u32 value)
+{
+	struct adrf5740_state *st = iio_priv(indio_dev);
+        // unsigned long values[BITS_PER_TYPE(value)=32];
+	DECLARE_BITMAP(values, BITS_PER_TYPE(value));
+
+	values[0] = value;
+
+        /* memory mapped vs or bus controlled */
+	gpiod_set_array_value_cansleep(st->gpios->ndescs, st->gpios->desc,
+				       NULL, values);
+	return 0;
+}
+
+static int adrf5740_write_raw(struct iio_dev *indio_dev,
+				 struct iio_chan_spec const *chan,
+				 int val,
+				 int val2,
+				 long mask)
+{
+        struct adrf5740_state *st = iio_priv(indio_dev);
+        struct adrf5740_chip_info *inf = st->chip_info;
+        int code = 0, gain;
+	
+        /* round to the nearest greatest integer */
+	if (val < 0)
+		gain = (val * 1000) - (val2 / 1000);
+	else
+		gain = (val * 1000) + (val2 / 1000);
+
+	if (abs(gain) > inf->gain_max || abs(gain) < inf->gain_min)
+		return -EINVAL;
+        
+        
+	switch (mask) {
+	case IIO_CHAN_INFO_HARDWAREGAIN:
+                code = (abs(gain) / 2000) & 0xF;
+                st->gain = code;
+                adrf5740_write(indio_dev, st->gain);
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static int adrf5740_write_raw_get_fmt(struct iio_dev *indio_dev,
+				     struct iio_chan_spec const *chan,
+				     long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_HARDWAREGAIN:
+		return IIO_VAL_INT_PLUS_MICRO_DB;
+	default:
+		return -EINVAL;
+	}
+}
+
+static const struct iio_info adrf5740_info = {
+	.read_raw = &adrf5740_read_raw,
+	.write_raw = &adrf5740_write_raw,
+	.write_raw_get_fmt = &adrf5740_write_raw_get_fmt,
+};
 
 
 static const struct of_device_id adrf5740_of_match[] = {
@@ -20,12 +147,33 @@ MODULE_DEVICE_TABLE(of, adrf5740_of_match);
 
 static int adrf5740_probe(struct platform_device *pdev){
        struct iio_dev *indio_dev;
+       struct adrf5740_state *st;
+
         indio_dev = devm_iio_device_alloc(&pdev->dev, 0);
         if(!indio_dev)
                 return -ENOMEM;
-        
-        indio_dev->name = "adrf5740";
+
+        st = iio_priv(indio_dev);
+        st->chip_info = &adrf5740_chip_info;
+        st->gain = st->chip_info->default_gain;
+       
+        indio_dev->name = st->chip_info->name;
         indio_dev->info = &adrf5740_info;
+        indio_dev->num_channels = st->chip_info->num_channels;
+        indio_dev->channels = st->chip_info->channels;
+        indio_dev->info = &adrf5740_info;
+
+        st->gpios = devm_gpiod_get_array(&pdev->dev, "ctrl", GPIOD_OUT_LOW);
+	if (IS_ERR(st->gpios))
+		return dev_err_probe(&pdev->dev, PTR_ERR(st->gpios),
+				     "failed to get gpios\n");
+
+	if (st->gpios->ndescs != st->chip_info->num_gpios) {
+		dev_err(&pdev->dev, "%d GPIOs needed to operate\n",
+			st->chip_info->num_gpios);
+		return -ENODEV;
+	}
+
         dev_info(&pdev->dev, "adrf5740 Probed");
         return devm_iio_device_register(&pdev->dev, indio_dev);
 }
